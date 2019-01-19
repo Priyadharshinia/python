d42599a7894cefa9569f34b45a4d075166ec8801
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-01-20 03:51:53 +0530

    Create Odddigit.c

---
+++ Odddigit.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int rev=0,r,d,n;
+scanf("%d",&n);
+while(n>0)
+{
+d=n%10;
+rev=rev*10+d;
+n=n/10;
+}
+while(rev!=0)
+{
+r=rev%10;
+if(r%2!=0)
+{
+printf("%d\t",r);
+}
+else
+{
+printf("");
+}
+rev=rev/10;
+}
+return 0;
+}
