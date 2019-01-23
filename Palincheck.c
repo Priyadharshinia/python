4dddc7301a0ea045a1cb51d1f89b7d917ee5a537
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-01-23 21:32:27 +0530

    Create Palincheck.c

---
+++ Palincheck.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int n,s=0,rev=0,p,d,r;
+scanf("%d",&n);
+while(n>0)
+{
+r=n%10;
+s=s+r;
+n=n/10;
+}
+s=p;
+while(p!=0)
+{
+d=p%10;
+rev=rev*10+d;
+p=p/10;
+}
+if(s==rev)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
