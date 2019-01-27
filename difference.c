8615e4aaa70751c149e2f9cb4049bf8fea258b33
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-01-27 10:24:12 +0530

    Update difference.c

--- difference.c
+++ difference.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
 int main()
 {
-int a[100],b[200],i,n;
+int a[100],b[100],i,n,j;
 scanf("%d",&n);
 for(i=0;i<n;i++) 
 {
 scanf("%d",&a[i]);
 } 
-for(i=0;i<n;i++) 
+for(j=0;j<n;j++) 
 {
-scanf("%d",&b[i]);
+scanf("%d",&b[j]);
 }  
 for(i=0;i<n;i++) 
+  {
+  for(j=0;j<n;j++)
 {
-if(a[i]==b[i]) 
+if(a[i]==b[j]) 
 { 
 printf("%d ",a[i]);
-} }
+} 
+  }}
 return 0;
 }
