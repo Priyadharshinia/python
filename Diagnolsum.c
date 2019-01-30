3d3943798c71e2d0c6569fd8735843800b903110
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-01-30 21:21:15 +0530

    Create Diagnolsum.c

---
+++ Diagnolsum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a[50][50],N,i,j,sum=0;
+scanf("%d",&N);
+for(i=0;i<N;i++)
+{
+for(j=0;j<N;j++)
+{
+scanf("%d",&a[i][j]);
+}
+}
+for(i=0;i<N;i++)
+{
+for(j=0;j<N;j++) 
+{
+if(i==j)
+{
+sum=sum+a[i][j];
+}
+}
+}
+printf("%d",sum);
+return 0;
+}
