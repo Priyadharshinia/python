2ac02a6694d7403cb9e884a4955ddf351463c980
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-05 23:37:46 +0530

    Create Rrepeat.c

---
+++ Rrepeat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void) 
+{
+	char a[100];
+int i,n,count=0,j;
+   scanf("%s",a); 
+for(i=1;a[i]!='\0';i++)
+{
+for(j=1;a[j]!='\0';j++) 
+{ 
+ 
+  if(a[i]==a[j]) 
+{
+  count++;
+}
+} }
+if(count>=1)
+{
+ printf("yes");
+}
+else 
+{
+  printf("no");
+}
+	return 0;
+}
