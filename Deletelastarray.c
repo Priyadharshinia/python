fbd6da6169d340693c807465f3cc78302506436e
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-07 13:24:52 +0530

    Update Deletelastarray.c

--- Deletelastarray.c
+++ Deletelastarray.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(void) 
- {
-	int A[100],i,n,k;
-scanf("%d%d",&n,&k);
+{
+int A[100],i,j,n,k;
+scanf("%d%d",&n,&k); 
+j=n-k;
 for(i=0;i<n;i++) 
 { 
 scanf("%d",&A[i]);
-} 	
-		
+} 			
 for(i=0;i<j;i++) 
 {
 printf("%d ",A[i]);
 }
-	return 0;
+return 0;
 }
  
