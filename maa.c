cc09326ea01803e399a359a87f6cd234a92a9adc
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-08 11:33:04 +0530

    Create maa.c

---
+++ maa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+int a[100];
+int max,i; 
+for(i=0;i<10;i++) 
+{ 
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<10;i++) 
+{  
+  if(max<a[i]) 
+  { 
+     max=a[i];
+  }
+} 
+printf("%d",max);
+	return 0;
+}
