fe78a16fb8363860e362b8e474b0162c987c954c
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-08 10:37:47 +0530

    Create Pow2.c

---
+++ Pow2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) 
+{
+int a,i,count=0;
+scanf("%d",&a);  
+for(i=1;i<a;i++)
+{ 
+if(i*i==a) 
+{ 
+count++;
+}
+} 
+if(count==1)
+{ 
+printf("yes");
+} 
+else 
+{ 
+printf("no");
+}
+	return 0;
+} 
