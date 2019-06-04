02414324df783ad33d75dcb9fdb730de52408a7a
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-06-04 01:21:54 -0700

    Create consectivecharacters.py

---
+++ consectivecharacters.py
@@ -0,0 +1,11 @@
+#priya dharshini
+a,b=map(str,input().split())
+count=0
+for i in range(0,len(a)):
+	for j in range(0,len(b)):
+		if a[i]==b[j]:
+			count+=1
+if count>=2:
+	print("yes")
+else:
+	print("no")
