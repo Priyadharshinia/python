c706382501e4a3935b57ada8d4ffef2d6b4db508
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-06-18 14:48:57 +0530

    Create Sorting.py

---
+++ Sorting.py
@@ -0,0 +1,28 @@
+#priyadharshini
+rat=int(input())
+
+gat=[]
+ 
+for i in range(rat):
+ 
+    li=[]
+ 
+    li=list(map(int,input().split()))
+ 
+    gat.append(li)
+ 
+ 
+ 
+fat=[]   
+ 
+for i in gat:
+ 
+    for j in i:
+ 
+        fat.append(j)
+ 
+fat.sort()
+ 
+for i in fat:
+ 
+    print(i,end=" ")
